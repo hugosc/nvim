564923dd7b82dd727598a9a520cf386ba231b830
Crocodile <hugo.serranochan@gmail.com>
2025-11-03 13:55:36 +0000

    fix: file reload on agent change

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,7 +11,32 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end, 100)
   end,
 })
+vim.api.nvim_create_autocmd("FileChangedRO", {
+  callback = function()
+    vim.cmd('echohl WarningMsg | echo "File changed RO." | echohl None')
+  end,
+})
 
+vim.api.nvim_create_autocmd("FileChangedShell", {
+  callback = function()
+    vim.opt.autoread = true
+    vim.cmd("checktime")
+    vim.cmd('echohl WarningMsg | echo "File changed shell. Auto-reloading..." | echohl None')
+  end,
+})
+
+-- More aggressive file change detection
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    if vim.bo.modifiable and not vim.bo.readonly then
+      vim.cmd("silent! checktime")
+    end
+  end,
+})
+
+-- Set global options for better file change handling
+vim.opt.autoread = true
+vim.opt.autowrite = true
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "NvimTree",
   callback = function(args)
@@ -39,6 +64,8 @@ vim.api.nvim_create_autocmd("FocusGained", {
     if loaded_content ~= "" then
       vim.fn.setreg('"', loaded_content)
     end
+    -- Check for external file changes when focus is gained
+    vim.cmd("checktime")
   end,
 })
 
